8a3c34d251b1d22a32f6dc074b087f72545fbbba
unknown <jackjcherry@hotmail.co.uk>
2022-10-01 15:31:36 +0100

    scroll button functionality hooked up, needs to be sticky at bottom of page and needs to conditionally rendered

--- cocktailapi/src/Components/ScrollButton/ScrollButton.js
+++ cocktailapi/src/Components/ScrollButton/ScrollButton.js
@@ -1,7 +1,7 @@
 function ScrollButton() {
     return (
         <div className="scroll_container">
-            <button className="scroll_button">Back to top</button>
+            <button className="scroll_button" onClick={() => {window.scrollTo({top: 0, behavior: 'smooth'});}}>Back to top</button>
         </div>
     )
 }
